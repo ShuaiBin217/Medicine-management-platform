6b3a01bdcba602af18ce55f42c02056a29e110e9
shuaibinbinbin <2967952827@qq.com>
2026-08-29 19:57:02 +0800

    Add Vercel and Render deployment config, use env var for API URL

--- wms-web/src/main.js
+++ wms-web/src/main.js
@@ -8,7 +8,7 @@ import VueRouter from 'vue-router';
 import router from './router';
 import store from './store';
 Vue.prototype.$axios=axios;
-Vue.prototype.$httpUrl='http://localhost:8090'
+Vue.prototype.$httpUrl = process.env.VUE_APP_API_URL || 'http://localhost:8090'
 Vue.config.productionTip = false
 //Vue.use(ElementUI);
 Vue.use(VueRouter);
