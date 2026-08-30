8c0292945c915458bfd021f929bfe9beab2bac9e
shuaibinbinbin <2967952827@qq.com>
2026-08-30 23:05:14 +0800

    update vue.config.js for server deployment

--- wms-web/vue.config.js
+++ wms-web/vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Medicine-management-platform/'
-    : '/',
+  publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false
